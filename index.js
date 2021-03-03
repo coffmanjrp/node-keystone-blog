76b146f9c1bf80bd4b265cee826928e4e91e65f6
coffmanjrp <coffmanjrp@gmail.com>
2021-03-03 11:17:24 -0800

    Create keystone app and initial settings

---
+++ index.js
@@ -0,0 +1,20 @@
+const dotenv = require('dotenv').config();
+const { Keystone } = require('@keystonejs/keystone');
+const { GraphQLApp } = require('@keystonejs/app-graphql');
+const { AdminUIApp } = require('@keystonejs/app-admin-ui');
+const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
+const PROJECT_NAME = 'Keystone Blog';
+const adapterConfig = { mongoUri: process.env.MONGO_URL };
+
+const keystone = new Keystone({
+  adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
+});
+
+module.exports = {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true }),
+  ],
+};
